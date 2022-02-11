e99f9b0299b48af8409a2de73e57eaec0f8e2b45
Mohamed H. Aly <94052626+Metwesh@users.noreply.github.com>
2022-02-12 00:15:19 +0200

    slight modification

--- includes/models/Api.php
+++ includes/models/Api.php
@@ -32,7 +32,7 @@ class Api
             } else {
                 return false;
             }
-        } catch (\Throwable) {
+        } catch (Throwable) {
             echo "Product SKU already exists in database. Reirecting...";
             header("HTTP/1.1 406 Error inserting product");
             header("refresh:3;url=http://localhost:3000/add-product");
@@ -50,7 +50,7 @@ class Api
             } else {
                 return false;
             }
-        } catch (\Throwable) {
+        } catch (Throwable) {
             echo "Error deleting product from database. Reirecting...";
             header("HTTP/1.1 406 Error deleting product from database");
             header("refresh:3;url=http://localhost:3000/add-product");
