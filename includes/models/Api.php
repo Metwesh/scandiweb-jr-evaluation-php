b9e189db59f2dec939bc2be05035282eea430d71
Mohamed H. Aly <94052626+Metwesh@users.noreply.github.com>
2022-02-25 13:45:50 +0200

    Attempt at fixing foreach

--- includes/models/Api.php
+++ includes/models/Api.php
@@ -40,16 +40,12 @@ class Api
         }
     }
 
-    public function deleteProduct($product_sku, $post)
+    public function deleteProduct($product_sku)
     {
         try {
-            $this->db->query("DELETE FROM products WHERE product_sku IN (" . $product_sku . ")");
+            $this->db->query("DELETE FROM products WHERE product_sku IN (:sku)");
+            $this->db->bind(":sku", $product_sku);
 
-            // $this->db->bind(":sku", $product_sku);
-            // 
-            foreach ($product_sku as $k => $post)
-                $this->db->bind(($k + 1), $post);
-            // 
             if ($this->db->execute()) {
                 return true;
             } else {
