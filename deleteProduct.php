7f5fbd4d82764cee21cfe96c67ea3f191d110adb
Mohamed H. Aly <94052626+Metwesh@users.noreply.github.com>
2022-02-26 14:25:40 +0200

    Added Validation & moved all validation to that class

--- deleteProduct.php
+++ deleteProduct.php
@@ -4,16 +4,13 @@ require_once('includes/initialize.php');
 
 header('Content-Type: application/json; charset=UTF-8');
 
-$response = array();
+$response = [];
 
-if (!isset($_POST['sku'])) {
-    header("HTTP/1.1 422 Required parameters missing");
-    header("refresh:3;url=https://scandiweb-product-page.herokuapp.com/");
-    echo 'Please select one or more of the checkboxes. Reirecting...';
-    exit();
-}
+Validator::checkEmptySKU($_POST['sku']);
 
-$sku = "'" . join("','", $_POST['sku']) . "'";
+$sku = $product->formatDeleteSKU($_POST['sku']);
+
+// $sku = "'" . join("','", $_POST['sku']) . "'";
 
 $result = $api->deleteProduct($sku);
 
