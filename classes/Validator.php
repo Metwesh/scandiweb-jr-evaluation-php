47f9cdb6405f77c06e1d4dd4d643872423b114b1
Mohamed H. Aly <94052626+Metwesh@users.noreply.github.com>
2022-02-27 21:03:57 +0200

    Validation debugging

--- classes/Validator.php
+++ classes/Validator.php
@@ -61,7 +61,7 @@ class Validator
 
     private function validatePrice($price)
     {
-        if (!is_float($price) || !is_int($price)) {
+        if (!is_float((int)$price) || !is_int((int)$price)) {
             self::$errors += ['Price' => 'Price can only contain numbers'];
         }
     }
@@ -71,13 +71,13 @@ class Validator
         switch ($description) {
             case is_array($description):
                 foreach ($description as $key) {
-                    if (!is_float($key) || !is_int($key)) self::$errors += ['Dimensions' => 'Dimensions can only contain numbers'];
+                    if (!is_float((int)$key) || !is_int((int)$key)) self::$errors += ['Dimensions' => 'Dimensions can only contain numbers'];
                 }
                 unset($key);
                 break;
 
             default:
-                if (!is_float($description) || !is_int($description)) self::$errors += ['Description' => 'Description can only contain numbers'];
+                if (!is_float((int)$description) || !is_int((int)$description)) self::$errors += ['Description' => 'Description can only contain numbers'];
                 break;
         }
     }
